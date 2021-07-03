df230761552280424dd5178595b3aa28fe6094ae
joungeun <lje0818ovo@naver.com>
2021-07-03 13:18:40 +0900

    [ADD] screen home finish and picture size adjustment

--- flutter_app/lib/screen/screen_home.dart
+++ flutter_app/lib/screen/screen_home.dart
@@ -24,10 +24,83 @@ class _HomeScreenState extends State<HomeScreen>{
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Center(child: Image.asset('images/cat.jpg', width: width *0.8),)
+              Center(
+                child: Image.asset(
+                    'images/cat.jpg',
+                    width: width *0.6
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(width * 0.024),
+              ),
+              Text(
+                '퀴즈 앱',
+                style: TextStyle(
+                  fontSize: width *0.065,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '퀴즈를 풀기 전 안내사항입니다.\n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.',
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: EdgeInsets.all(width*0.048),
+              ),
+              _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+              _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤 다음 문제 버튼을 \n눌러주세요.'),
+              _buildStep(width, '3. 만점을 향해 도전해보세요'),
+              Padding(
+                padding: EdgeInsets.all(width * 0.048),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: width * 0.036),
+                child: Center(
+                  child: ButtonTheme(
+                    minWidth: width * 0.8,
+                    height: height * 0.05,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ElevatedButton(
+                        child: Text(
+                          '지금 퀴즈 풀기',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
+                        ),
+                        onPressed: (){},
+                    ),
+                  ),
+                )
+              )
             ],
           )
         ),
+    );
+  }
+
+  Widget _buildStep(double width, String title){
+    return Container(
+      padding: EdgeInsets.fromLTRB(
+        width * 0.048,
+        width * 0.024,
+        width *0.048,
+        width*0.024,
+      ),
+      child:Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(
+            Icons.check_box,
+            size: width*0.04,
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: width * 0.024),
+          ),
+          Text(title),
+        ],
+      )
     );
   }
 }
