0c2a08530fbb7fba73eb260940060c168a199798
joungeun <lje0818ovo@naver.com>
2021-08-20 16:08:14 +0900

    [ADD] Check score page

--- flutter_app/lib/screen/screen_result.dart
+++ flutter_app/lib/screen/screen_result.dart
@@ -36,7 +36,53 @@ class ResultScreen extends StatelessWidget{
           width: width * 0.85,
           height: height * 0.5,
           child: Column(
-            children: <Widget>[],
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: width * 0.048),
+              ),
+              Container(decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Colors.deepPurple),
+                color: Colors.white,
+              ),
+              width: width *0.73,
+              height: height * 0.25,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(
+                        0, width * 0.048, 0, width * 0.012),
+                    child: Text(
+                      '수고하셨습니다.',
+                      style: TextStyle(
+                          fontSize: width * 0.055,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                  ),
+                  Text('당신의 점수는', style: TextStyle(
+                    fontSize: width * 0.048,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                  Text(
+                    score.toString() + '/' + quizs.length.toString(),
+                    style: TextStyle(
+                      fontSize: width * 0.15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(width * 0.012)
+                  )
+                ],
+              )
+              )
+            ],
           )
         )
       )
