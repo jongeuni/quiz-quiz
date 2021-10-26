7c6dc2bedaa142fa4b6f65de22a86f5cd3ed29c8
joungeun <lje0818ovo@naver.com>
2021-10-26 22:29:57 +0900

    [ADD] 네비게이션 테스트 페이지

---
+++ quiz_app_v2/lib/screen/choice.dart
@@ -0,0 +1,14 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class Chocie extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("data"),
+      //bottomNavigationBar: buildCurvedNavigationBar(),
+    );
+  }
+}
