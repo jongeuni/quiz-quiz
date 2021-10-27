7364b5b01da66c4478db0534a542fa942c74af88
joungeun <lje0818ovo@naver.com>
2021-10-27 09:05:38 +0900

    [FIX] 기본값이 Dropdown Box에 뜨도록 수정

--- quiz_app_v2/lib/screen/choice.dart
+++ quiz_app_v2/lib/screen/choice.dart
@@ -5,30 +5,27 @@ import 'package:flutter_application_1/screen/home_screen.dart';
 
 
 class Chocie extends StatelessWidget {
+  final _valueList = ['동물','노래','영화','기타'];
   var _selectedValue = '기타';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(130.0),
+        padding: const EdgeInsets.all(30.0),
         child: Center(
-          child: Column(children: [
-        DropdownButton<String>(icon: const Icon(Icons.arrow_downward),
-        items: <String>['동물','노래','영화','기타'].map((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-            
-            );
-        }).toList(),
-        onChanged: (value){
-          
-        },
-        )
-      ],),),)
+          child: DropdownButton(
+            value: _selectedValue,
+            items: _valueList.map((value){
+              return DropdownMenuItem(
+                value: value,
+                child: Text(value),);
+            },
+          ).toList(),
+          onChanged: (value){
+          },)
     
       //bottomNavigationBar: buildCurvedNavigationBar(),
-    );
+    )));
   }
 }
 
