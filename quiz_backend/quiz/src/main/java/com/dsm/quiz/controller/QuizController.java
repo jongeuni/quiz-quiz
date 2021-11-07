b098a9f19afeaa217b492c075d44b503b8634d9e
joungeun <lje0818ovo@naver.com>
2021-11-07 21:34:36 +0900

    [ADD] Explanation about the quiz return API

--- quiz_backend/quiz/src/main/java/com/dsm/quiz/controller/QuizController.java
+++ quiz_backend/quiz/src/main/java/com/dsm/quiz/controller/QuizController.java
@@ -2,6 +2,10 @@ package com.dsm.quiz.controller;
 
 import com.dsm.quiz.payload.response.QuizResponse;
 import com.dsm.quiz.service.QuizService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +14,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@Api
 @RequiredArgsConstructor
 @Controller
 public class QuizController {
     private final QuizService quizService;
 
+    @ApiOperation(value = "퀴즈를 리스트 반환")
     @ResponseBody
     @GetMapping("quiz/{num}")
-    public List<QuizResponse> quizReturn(@PathVariable int num){
+    @ApiResponse(code=200, message = "퀴즈 리턴됨", response = QuizResponse.class, responseContainer = "List")
+    public List<QuizResponse> quizReturn(@ApiParam(value = "Quiz count", required = true) @PathVariable int num){
         return quizService.randomQuiz(num);
     }
 }
--- quiz_backend/quiz/src/main/java/com/dsm/quiz/payload/response/QuizResponse.java
+++ quiz_backend/quiz/src/main/java/com/dsm/quiz/payload/response/QuizResponse.java
@@ -1,5 +1,6 @@
 package com.dsm.quiz.payload.response;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -8,7 +9,10 @@ import lombok.ToString;
 @Builder
 @Getter
 public class QuizResponse {
+    @ApiModelProperty(example = "다음 중 동물 종류가 아닌 것은?")
     private String title;
+    @ApiModelProperty(example = "코기린/코끼리/고양이/강아지")
     private String content;
+    @ApiModelProperty(example = "0")
     private int answer;
 }
