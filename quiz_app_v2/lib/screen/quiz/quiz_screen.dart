42f92f66879894a73650fac722ee3c2a22c04372
joungeun <lje0818ovo@naver.com>
2021-11-30 22:07:26 +0900

    [ADD] Quiz 페이지 구조

--- quiz_app_v2/lib/screen/quiz/quiz_screen.dart
+++ quiz_app_v2/lib/screen/quiz/quiz_screen.dart
@@ -1,5 +1,7 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 
 class QuizScreen extends StatefulWidget{
@@ -13,7 +15,59 @@ class _QuizScreenState extends State<QuizScreen>{
   _QuizScreenState(this._selectedValue);
   @override
   Widget build(BuildContext context){
-    return SafeArea(child: Text(_selectedValue)
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+
+
+    return Container(
+      //decoration: BorderRadius.circular(20),
+      child: Column(children: <Widget>[
+        Container(
+          padding: EdgeInsets.fromLTRB(0,width * 0.024, 0, width * 0.024),
+          child: Text(
+        'Q'+'질문입니당',
+        style: TextStyle(
+          fontSize: width * 0.05,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+        ),
+        Container(
+          width: width * 0.8, // ?
+            padding: EdgeInsets.only(top: width * 0.012),
+            child:  AutoSizeText(
+              "quiz.title",
+              textAlign: TextAlign.center,
+              maxLines: 2, // ?
+              style: TextStyle(
+                fontSize: width * 0.04,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            
+        ),
+        Expanded(child: Container(),),
+        Column(children: _buildCandidates(width, _selectedValue),),
+        Container(
+          padding: EdgeInsets.all(width * 0.024),
+          
+        )
+      ])
+      
+    
     );
+   // return SafeArea(child: Text(_selectedValue));
   }
+  List<Widget> _buildCandidates(double width, String value){
+    List<Widget> children = [];
+    for(int i = 0; i<4; i++){
+      children.add(
+        Text(value)
+      );
+
+    }
+    return children;
+  }
+
 }
